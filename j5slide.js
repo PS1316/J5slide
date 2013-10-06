8e2335f5c0a5d25c9bcac333f7157d5acc34c1c1
PS1316 <17268628@student.uws.edu.au>
2013-10-06 18:49:27 +1100

    Extra scaling
    Added prescaling of slide content
    Added navigation buttons for first and last slides
    Updated fullscreen icon
    Reverted styling to light text on dark background

--- j5slide.js
+++ j5slide.js
@@ -43,14 +43,17 @@ J5.loadIframe = function() {
     
     // add slide viewer UI controls
     frame.height = this.height = parseInt(this.height + 30);
-    var noteButton = J5.notesEnabled ? "<button id='togglenotes' title='Toggle Notes' onclick='parent.J5.displayNotes()'><svg viewBox='0 0 512 512' width='20px' height='20px'><path d='M444.125,135.765 L149.953,429.937 l-67.875-67.875 L376.219,67.859 L444.125,135.765 z M444.125,0 l-45.281,45.234 l67.906,67.906 L512,67.859 L444.125,0 z M66.063,391.312 L0,512 l120.703-66.063 L66.063,391.312 z'/></svg></button>" : "";
+    var noteButton = J5.notesEnabled ? "<button id='togglenotes' title='Toggle Notes' onclick='parent.J5.displayNotes()'><svg viewBox='0 0 512 512' width='20px' height='20px'><path d='M444.125,135.765 L149.953,429.937 l-67.875-67.875 L376.219,67.859 L444.125,135.765 Z M444.125,0 l-45.281,45.234 l67.906,67.906 L512,67.859 L444.125,0 Z M66.063,391.312 L0,512 l120.703-66.063 L66.063,391.312 Z'/></svg></button>" : "";
     var controls = doc.body.appendChild(document.createElement("div"));
     controls.id = "controls";
-    controls.innerHTML = "<button id='prev' title='Previous Slide' onclick='parent.J5.backMsg()'><svg viewBox='0 0 50 50' width='20px' height='20px'><path id='lol' d='M47,3 L24,25 L47,47 H27 L4,25 L27,3 H47 Z'></path></svg></button>" +
+    controls.innerHTML = "<button id='first' title='First Slide' onclick='parent.J5.goToSlide(1)'><svg viewBox='0 0 50 50' width='20px' height='20px'><path d='M0,3 H4 V47 H0 V3 Z M50,3 L27,25 L50,47 H30 L7,25 L30,3 H50 Z'></path></svg></button>" +
+                         "<button id='prev' title='Previous Slide' onclick='parent.J5.backMsg()'><svg viewBox='0 0 50 50' width='20px' height='20px'><path d='M47,3 L24,25 L47,47 H27 L4,25 L27,3 H47 Z'></path></svg></button>" +
                          "<button id='next' title='Next Slide' onclick='parent.J5.forwardMsg()'><svg viewBox='0 0 50 50' width='20px' height='20px'><path d='M3,3 L26,25 L3,47 H23 L46,25 L23,3 H3 Z'></path></svg></button>" +
-                         "<p id='leftcontrols'><input onchange='parent.J5.setSlide(this.value)' id='slideidx' maxlength='3' value='0'> /<span id='slidecount'>...</span></p>" +
+                         "<button id='last' title='Last Slide' onclick='parent.J5.goToSlide(this.parentNode.querySelector(\"#slidecount\").innerHTML)'><svg viewBox='0 0 50 50' width='20px' height='20px'><path d='M50,3 H46 V47 H50 V3 Z M0,3 L23,25 L0,47 H20 L43,25 L20,3 H0 Z'></path></svg></button>" +
+                         "<p id='leftcontrols'><input onchange='parent.J5.goToSlide(this.value)' id='slideidx' maxlength='3' value='0'> /<span id='slidecount'>...</span></p>" +
                          "<p id='rightcontrols'>" + noteButton +
-                         "<button id='fullscreen' title='Fullscreen' onclick='parent.J5.fullscreen()'><svg viewBox='0 0 512 512' width='20px' height='20px'><path d='M187.765,417.257L232.431,462H50V279.572l44.744,44.666l73.07-73.07l93.02,93.02L187.765,417.257z M279.572,50l44.666,44.743l-71.571,71.572l93.02,93.02l71.572-71.572L462,232.428V50H279.572z'/></svg></button></p>";
+                         "<button id='fullscreen' title='Fullscreen' onclick='parent.J5.fullscreen()'><svg viewBox='0 0 512 512' width='20px' height='20px'><path d='M130,210 L40,120 L0,160 L0,0 L160,0 L120,40 L210,130 Z M382,210 L472,120 L512,160 L512,0 L352,0 L392,40 L302,130 Z M382,302 L472,392 L512,352 L512,512 L352,512 L392,472 L302,382 Z M210,382 L120,472 L160,512 L0,512 L0,352 L40,392 L130,302 Z'/></svg></button></p>";
+                         //M187.765,417.257 L232.431,462 H50 V279.572 l44.744,44.666 l73.07-73.07l93.02,93.02 L187.765,417.257 Z M279.572,50 l44.666,44.743 l-71.571,71.572 l93.02,93.02 l71.572-71.572 L462,232.428 V50 H279.572 Z
                          // placeholder icon - <svg viewBox='0 0 50 50' width='20px' height='20px'><path d='M3,3 L47,3 L47,47 L3,47 L3,3 Z'/></svg>
     
     // add slide note box
@@ -82,15 +85,15 @@ J5.loadIframe = function() {
         args.forEach(function(e, i, a) { a[i] = decodeURIComponent(e) });
         if (args[0] == "LOCATION" && argc == 2) {
             J5.idx = parseInt(args[1]);
-            doc.querySelector("#slideidx").value = J5.idx;
-            doc.querySelector("#prev").disabled = J5.idx == 1;
-            doc.querySelector("#next").disabled = J5.idx == J5.slides.length;
+            doc.getElementById("slideidx").value = J5.idx;
+            doc.getElementById("first").disabled = doc.getElementById("prev").disabled = J5.idx == 1;
+            doc.getElementById("last").disabled = doc.getElementById("next").disabled = J5.idx == J5.slides.length;
         }
         if (args[0] == "REGISTERED" && argc == 2) {
-            doc.querySelector("#slidecount").innerHTML = args[1];
+            doc.getElementById("slidecount").innerHTML = args[1];
         }
         if (args[0] == "NOTES" && argc == 2) {
-            doc.querySelector("#notes").innerHTML = args[1];
+            doc.getElementById("notes").innerHTML = args[1];
         }
     }
     
@@ -116,17 +119,17 @@ J5.loadSlideFrame = function() {
                  "a:hover { text-decoration: underline; }" +
                  "footer { position: absolute; bottom: 50px; right: 50px; }" +
                  "strong { color: #0066FF; }" +
-                 "body { font-family: 'Oswald', arial, serif; background-color: white; color: black; font-size: 30px; line-height: 120%; }" +
+                 "body { font-family: 'Oswald', arial, serif; background-color: #1C1C1C; color: white; font-size: 30px; line-height: 120%; }" +
                  "img { margin: 0 auto; display: block; }" +
                  "p img { position: relative; left: -20px }" +
                  "section { -moz-transition: left 400ms linear 0s; -webkit-transition: left 400ms linear 0s; -ms-transition: left 400ms linear 0s; transition: left 400ms linear 0s; }" + // transition effect
-                 "section { background: white; overflow: hidden; left: -150%; }" +
-                 "section[slide-selected] { left: 0; }" +
-                 "section[slide-selected] ~ section { left: +150%; }" +
+                 "section { background: #1C1C1C; overflow: hidden; left: -150%; }" +
+                 "section[aria-selected] { left: 0; }" +
+                 "section[aria-selected] ~ section { left: +150%; }" +
                  "h1 { color: #FF6600; margin: 20px 0; font-size: 50px; text-align: center; padding: 0 10px; line-height: 100% }" +
                  "h2 { color: #FF0066; margin: 20px 0; font-size: 40px; text-align: center; padding: 0 10px; line-height: 100% }" +
-                 "ul { margin: 10px 0 0 100px; font-size: 0.9em; width: 750px; line-height: 100%; display: inline-block; }" +
-                 "q, p { padding: 10px 20px; }" +
+                 "ul { margin: 10px 0 0 50px; font-size: 0.9em; width: 750px; line-height: 100%; display: inline-block; }" +
+                 "q, p { padding: 5px 20px; }" +
                  "q:after { content: ''; }" +
                  "q:before { content: ''; }" +
                  "q { display: block; margin-top: 140px; }" +
@@ -139,7 +142,7 @@ J5.loadSlideFrame = function() {
                  "body { width: 800px; height: 600px; margin-left: -400px; margin-top: -300px; position: absolute; top: 50%; left: 50%; overflow: hidden; }" +
                  "html { overflow: hidden; }" +
                  "section { position: absolute; pointer-events: none; width: 100%; height: 100%; }" +
-                 "section[slide-selected] { pointer-events: auto; }" +
+                 "section[aria-selected] { pointer-events: auto; }" +
                  "body { display: none }" +
                  "body.loaded { display: block }";
 
@@ -171,13 +174,11 @@ J5.loadSlideFrame = function() {
         if (e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 33) { 
             e.preventDefault();
             J5.back();
-            J5.postMsg(J5.iframe.contentWindow, "LOCATION", J5.idx);
         }
         // right arrow, down arrow or page down
         if ( e.keyCode == 39 || e.keyCode == 40 || e.keyCode == 34) {
             e.preventDefault();
             J5.forward();
-            J5.postMsg(J5.iframe.contentWindow, "LOCATION", J5.idx);
         }
     }
     
@@ -189,11 +190,9 @@ J5.loadSlideFrame = function() {
         }
         if (e.data == "BACK") {
             J5.back();
-            J5.postMsg(J5.iframe.contentWindow, "LOCATION", J5.idx);
         }
         if (e.data == "FORWARD") {
             J5.forward();
-            J5.postMsg(J5.iframe.contentWindow, "LOCATION", J5.idx);
         }
         if (e.data == "GET_NOTES") {
             J5.postMsg(J5.iframe.contentWindow, "NOTES", J5.getNotes(J5.idx - 1));
@@ -205,8 +204,8 @@ J5.loadSlideFrame = function() {
         if (J5.slides[i].parentNode) {
             //J5.slides[i].parentNode.removeChild(J5.slides[i]);
         }
-        var slides = doc.importNode(J5.slides[i], true);
-        doc.body.appendChild(slides);
+        var slide = doc.importNode(J5.slides[i], true);
+        doc.body.appendChild(slide);
     }
     
     // make image slides (<img class='fill'>) fill entire slide 
@@ -221,20 +220,69 @@ J5.loadSlideFrame = function() {
             }
         }
     }
+
+    // prescale slide content
+    prescale = function() {
+        var s = doc.querySelectorAll("section[typeof='http://purl.org/ontology/bibo/Slide']")
+        for (var i = 0; i < s.length; i++) {
+            if (s[i].scrollHeight > 600 || s[i].scrollWidth > 800) {
+                var images = s[i].getElementsByTagName("img");
+                for (var j = 0; j < images.length; j++) {
+                    if (images[j].height > 600 || images[j].width > 800) {
+                        var sx = images[j].scrollWidth / 800;
+                        var sy = images[j].scrollHeight / 600;
+                        var scaleAmount = 1 / Math.max(sx, sy);
+                        images[j].width = images[j].width * scaleAmount;
+                        images[j].height = images[j].height * scaleAmount;
+                    }
+                }
+                
+                var wrapper = document.createElement("div");
+                for (var j = 0; j < s[i].children.length;) {
+                    wrapper.appendChild(s[i].children[j]);
+                }
+                s[i].appendChild(wrapper);
+                var sx = s[i].scrollWidth / 800;
+                var sy = s[i].scrollHeight / 600;
+                var scaleAmount = 1 / Math.max(sx, sy);
+                if (scaleAmount < 0.5) scaleAmount = 0.5;
+                var transform = "scale(" + scaleAmount + ")";
+                wrapper.style.MozTransform = transform;
+                wrapper.style.WebkitTransform = transform;
+                wrapper.style.OTransform = transform;
+                wrapper.style.msTransform = transform;
+                wrapper.style.transform = transform;
+                wrapper.style.MozTransformOrigin = "center top";
+                wrapper.style.WebkitTransformOrigin = "center top";
+                wrapper.style.OTransformOrigin = "center top";
+                wrapper.style.msTransformOrigin = "center top";
+                wrapper.style.TransformOrigin= "center top";
+            }
+        }
+    }
     
     J5.view = frame.contentWindow;
-    
     J5.setSlide(J5.idx);
     doc.body.className = "loaded";
+    
+    if (frame.contentDocument.readyState == "complete") {
+        prescale();
+    }
+    else {
+        frame.contentWindow.onload = prescale;
+    }
+    
     frame.contentWindow.onresize();
 }
 
 J5.back = function() {
     this.setSlide(this.idx - 1);
+    this.postMsg(this.iframe.contentWindow, "LOCATION", this.idx);
 }
 
 J5.forward = function() {
     this.setSlide(this.idx + 1);
+    this.postMsg(this.iframe.contentWindow, "LOCATION", this.idx);
 }
 
 J5.backMsg = function() {
@@ -243,6 +291,11 @@ J5.backMsg = function() {
 
 J5.forwardMsg = function() {
     this.view.postMessage("FORWARD", "*");
+}
+
+J5.goToSlide = function(s) {
+    this.setSlide(s);
+    this.postMsg(this.iframe.contentWindow, "LOCATION", this.idx);
 }
 
 J5.postMsg = function(win, msg) {
@@ -256,16 +309,16 @@ J5.setSlide = function(idx) {
     if (idx < 1 || idx > this.slides.length) return;
     this.idx = idx;
     var frame = this.iframe.contentDocument.body.firstChild;
-    var old = frame.contentDocument.querySelector("section[slide-selected]");
+    var old = frame.contentDocument.querySelector("section[aria-selected]");
     var next = frame.contentDocument.querySelector("section[typeof='http://purl.org/ontology/bibo/Slide']:nth-of-type("+ this.idx +")");
     if (old) {
-        old.removeAttribute("slide-selected");
+        old.removeAttribute("aria-selected");
         old.style.cssText += ""; // force IE redraw
         var video = old.querySelector("slide-video");
         if (video) { video.pause(); }
     }
     if (next) {
-        next.setAttribute("slide-selected", "true");
+        next.setAttribute("aria-selected", "true");
         next.style.cssText += ""; // force IE redraw
         J5.view.postMessage("GET_NOTES", "*");
         var video = next.querySelector("slide-video");
@@ -294,7 +347,6 @@ J5.displayNotes = function() {
 
 J5.fullscreen = function() {
     var e = this.view.frameElement;
-	console.log(e);
     fullscreenAllowed = e.requestFullscreen || e.requestFullScreen || e.mozRequestFullScreen || e.webkitRequestFullScreen;
 	console.log(e.webkitRequestFullScreen);
 	console.log(fullscreenAllowed);
@@ -303,7 +355,7 @@ J5.fullscreen = function() {
     }
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("DOMContentLoaded", function() {
     J5.slides = document.querySelectorAll("section[typeof='http://purl.org/ontology/bibo/Slide']");
     if (J5.slides) {
         J5.loadIframe();
